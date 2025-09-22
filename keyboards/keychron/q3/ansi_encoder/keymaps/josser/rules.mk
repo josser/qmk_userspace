0ad8dca43b63ad4c135f07fa3a02ff7c09765d54
Dmytro Chyrkin <dmitry.chirkin@gmail.com>
2025-09-22 15:01:17 +0300

    fix: removed via support, updated deprecated keycodes

--- keyboards/keychron/q3/ansi_encoder/keymaps/josser/rules.mk
+++ keyboards/keychron/q3/ansi_encoder/keymaps/josser/rules.mk
@@ -1,7 +1,9 @@
-VIA_ENABLE = yes
+VIA_ENABLE = no
 ENCODER_MAP_ENABLE = yes
 MOUSEKEY_ENABLE = no
 MUSIC_ENABLE = no
+NKRO_ENABLE = yes
+COMMAND_ENABLE = no
 
 VPATH += keyboards/keychron/common
 SRC += keychron_common.c
